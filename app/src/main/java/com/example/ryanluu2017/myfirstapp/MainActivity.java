d0169a5a74b7dd4699f6ddc0b9f01684c1a8563f
Ryan Luu <ryanluu12345@gmail.com>
2018-02-02 03:03:24 -0800

    First working template

--- app/src/main/java/com/example/ryanluu2017/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/ryanluu2017/myfirstapp/MainActivity.java
@@ -22,26 +22,18 @@ public class MainActivity extends AppCompatActivity implements Serializable {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        this.foodItems=new ArrayList();
-        this.itemTv=(TextView) findViewById(R.id.foodItem);
+
     }
 
     // Method called when the addItem button is clicked
     public void onAddItem(View v){
 
-        //Adds grocery items to a previously initialized ArrayList
-        String addedItem=this.itemTv.getText().toString();
-        this.foodItems.add(addedItem);
 
     }
 
     // Method called when the viewList button is clicked
     public void onViewListClick(View v){
 
-        //Sets an intent to another activity. Inititates a change in activity
-        Intent viewListIntent=new Intent(this,ViewListActivity.class);
-        viewListIntent.putStringArrayListExtra("groceryList",this.foodItems);
-        startActivity(viewListIntent);
 
     }
 
